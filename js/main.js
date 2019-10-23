0b8caf63aff188fc7572571a26c8fab4c8740a41
xk0der <amit@xkoder.com>
2019-10-23 13:13:25 +1300

    Added JS folder

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+
+    function selectItem(evtObj) {
+        $('.js-master-container').removeClass('full-height');
+        $('.js-link').removeClass('clicked');
+        $(evtObj.currentTarget).addClass('clicked');
+        let item = $(evtObj.currentTarget).attr('href').replace('#', '');
+        $('.content').html($('.js-content-' + item).html());
+        $('.js-content-container').addClass('content').addClass('content-background');
+    }
+
+    $('.js-link').each(function (idx, obj) {
+        $(obj).click(function (evtObj) {
+            selectItem(evtObj);
+        });
+
+        $(obj).hover(function (evtObj) {
+            selectItem(evtObj);
+        });
+    });
+
+    let logoTopConstant = $('.js-logo-container').position().top;
+    let logoHeightConstant = $('.js-logo-container').height();
+
+    $(window).resize(function () {
+        setTimeout(function() {
+            logoTopConstant = $('.js-logo-container').position().top;
+            logoHeightConstant = $('.js-logo-container').height();
+        }, 100);
+    })
+
+    $(window).scroll(function () {
+        let logoTop = $('.js-logo-container').position().top;
+        let scrollTop = $(this).scrollTop();
+        let diff = scrollTop - logoTopConstant;
+
+        if (scrollTop > (logoTopConstant + logoHeightConstant)) {
+            $('.js-header').removeClass('d-none');
+            $('.js-header').hide();
+            $('.js-header').show(1000);
+        } else {
+            $('.js-header').addClass('d-none');
+            $('.js-header').hide(5000);
+        }
+    });
+
+});
